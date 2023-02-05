7eb987b3ae91cfb936db503a47fc93b9b52d73a5
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-02-05 10:02:57 -0500

    Refactor repeated code

--- lua/cstm-lsp-handlers.lua
+++ lua/cstm-lsp-handlers.lua
@@ -1,17 +1,25 @@
 local M = {}
 
-M._generic = function(err, result)
+local make_qf_entry = function(edit, bufnr)
+	local start_line = edit.range.start.line
+	local line = vim.api.nvim_buf_get_lines(bufnr, start_line, start_line + 1, true)[1]
+	return {
+		bufnr = bufnr,
+		lnum = start_line + 1,
+		col = edit.range.start.character + 1,
+		text = line,
+	}
+end
+
+M._generic = function(result)
+	P(result)
 	local entries = {}
 	if result.documentChanges then
 		for _, changes in ipairs(result.documentChanges) do
 			local bufnr = vim.uri_to_bufnr(changes.textDocument.uri)
 
 			for _, edit in ipairs(changes.edits) do
-				table.insert(entries, {
-					bufnr = bufnr,
-					lnum = edit.range.start.line + 1,
-					col = edit.range.start.character + 1,
-				})
+				table.insert(entries, make_qf_entry(edit, bufnr))
 			end
 		end
 	end
@@ -22,18 +30,14 @@ M._python = M._generic
 M._rust = M._generic
 
 -- Specialised function as the generated table is different for Lua
-M._lua = function(err, result)
+M._lua = function(result)
 	local entries = {}
 	if result.changes then
 		for uri, edits in pairs(result.changes) do
 			local bufnr = vim.uri_to_bufnr(uri)
 
 			for _, edit in ipairs(edits) do
-				table.insert(entries, {
-					bufnr = bufnr,
-					lnum = edit.range.start.line + 1,
-					col = edit.range.start.character + 1,
-				})
+				table.insert(entries, make_qf_entry(edit, bufnr))
 			end
 		end
 	end
@@ -51,7 +55,7 @@ M.rename_with_qflist = function()
 		end
 
 		local lang = vim.api.nvim_buf_get_option(0, "filetype")
-		local entries = M["_" .. lang](err, result)
+		local entries = M["_" .. lang](result)
 		vim.lsp.handlers["textDocument/rename"](err, result, ...)
 		vim.fn.setqflist(entries, "r")
 		vim.cmd("copen")
