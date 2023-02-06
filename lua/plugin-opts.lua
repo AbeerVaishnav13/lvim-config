c7ea169ce67732ac094784d26e3d806c6cef28c7
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2023-02-05 19:05:30 -0500

    Add treesitter playground plugin

--- lua/plugin-opts.lua
+++ lua/plugin-opts.lua
@@ -23,6 +23,9 @@ lvim.plugins = {
 	{
 		"dag/vim-fish",
 	},
+	{
+		"nvim-treesitter/playground",
+	},
 }
 
 -- Telescope setup
@@ -60,6 +63,7 @@ lvim.builtin.treesitter.ensure_installed = {
 	"lua",
 	"markdown",
 	"python",
+	"query",
 	"rust",
 	"typescript",
 	"yaml",
@@ -113,3 +117,25 @@ dap.configurations.python = {
 		end,
 	},
 }
+
+-- TS Playground setup
+require("nvim-treesitter.configs").setup({
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+})
